c3aa0e3f030fb3255b65585d1268c5754325ed0f
Rohan <rohan.canvas@gmail.com>
2022-08-22 09:46:38 -0700

    2nd commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -132,7 +132,12 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: [['allure', {outputDir: 'allure-results'}]],
+    reporters: ['spec', ['allure', {
+        outputDir: 'reports/allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+        useCucumberStepReporter: true
+    }]], //,['allure', {outputDir: 'allure-results'}],'cucumber','html'
 
 
     //
